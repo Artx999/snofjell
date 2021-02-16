d57afd5913955e3f0ff5aca583464b75a27b618c
Artx999 <54852665+Artx999@users.noreply.github.com>
2021-02-16 15:23:51 +0100

    Added style.css

--- structure/head.php
+++ structure/head.php
@@ -3,5 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $root ?>style.css">
     <script src="<?php echo $root ?>script.js"></script>
 </head>
